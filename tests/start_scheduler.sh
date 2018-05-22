38e2089d2fd1e1e5931852410a6df74d3b5bd849
hm <haiyangfu512@gmail.com>
2018-05-22 08:43:46 +0800

    with my start

---
+++ tests/start_scheduler.sh
@@ -0,0 +1,6 @@
+export DMLC_NUM_WORKER=1
+export DMLC_NUM_SERVER=1
+export DMLC_PS_ROOT_URI='127.0.0.1'
+export DMLC_PS_ROOT_PORT=8000
+export DMLC_ROLE='scheduler'
+./test_simple_app
---
+++ tests/start_test.sh
@@ -0,0 +1,2 @@
+#export PS_VERBOSE=100; ./local.sh 1 1 ./test_connection
+export PS_VERBOSE=100; ./local.sh 1 1 ./test_simple_app
